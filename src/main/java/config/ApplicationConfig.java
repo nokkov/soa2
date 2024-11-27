52ec6c9dfe1a64031b55fb58dfd3cc7f4124d0d9
nokkov <arkady-chesnokof@yandex.ru>
2024-11-27 11:33:04 +0300

    change config file

--- src/main/java/config/ApplicationConfig.java
+++ src/main/java/config/ApplicationConfig.java
@@ -1,9 +1,22 @@
 package config;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import controller.EyeColorResource;
+import controller.PersonResource;
 
 @ApplicationPath("api")
 public class ApplicationConfig extends Application {
     
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(EyeColorResource.class);
+        classes.add(PersonResource.class);
+        return classes;
+    }
 }
